53d2dfddabd1d477b6220156ea44d09aeecc35d5
Luís Mendes <jluis.lam1@gmail.com>
2023-06-09 00:34:15 +0100

    First

---
+++ app/src/main/java/pt/ipt/dama/dama_photocamera/MainActivity.kt
@@ -0,0 +1,46 @@
+package pt.ipt.dama.dama_photocamera
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+    lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        imageView = findViewById(R.id.imageView)
+
+        button.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    /**
+     * function to open the camera and take one photo
+     */
+    private fun takePhoto() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        // start the camera action
+        resultLauncher.launch(cameraIntent)
+    }
+
+    var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result -> if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            imageView.setImageBitmap(data?.extras?.get("data") as Bitmap)
+        }
+        }
+}
